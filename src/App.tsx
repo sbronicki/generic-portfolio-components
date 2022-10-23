75e801e14ae7797b493f24fa4e4f40573f31359c
sbronicki <sbronicki@gmail.com>
2022-10-23 19:49:53 -0400

    folder structure + antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Hero from "./Components/Hero";
 
 function App() {
-  return <div className="App">this is tsx!</div>;
+  return (
+    <div className="App">
+      <Hero />
+    </div>
+  );
 }
 
 export default App;
