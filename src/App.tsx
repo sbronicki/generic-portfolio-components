9da634ea188104589c9bc8265fce9342e6868082
sbronicki <sbronicki@gmail.com>
2022-10-23 20:39:45 -0400

    some nav stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import Hero from "./Components/Hero";
 
-function App() {
+import { Layout } from "antd";
+import { Nav } from "./Components/Nav/Nav";
+const { Header, Footer, Content } = Layout;
+
+const App: React.FC = () => {
+  const navItems = ["Home", "About", "Skills", "Education", "Contact"];
   return (
     <div className="App">
-      <Hero />
+      <Layout>
+        <Header>
+          <Nav navItems={navItems} />
+        </Header>
+        {/* <Content>
+          <Hero />
+        </Content>
+        <Footer>Footer</Footer> */}
+      </Layout>
     </div>
   );
-}
+};
 
 export default App;
