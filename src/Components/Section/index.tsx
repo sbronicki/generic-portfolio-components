c41cd5c2b18f0aa5ee9f885586105eceaa8a6a0d
sbronicki <sbronicki@gmail.com>
2022-11-06 14:13:51 -0500

    section mobile styling

--- src/Components/Section/index.tsx
+++ src/Components/Section/index.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import { Col, Row, Image, Typography, Divider } from "antd";
+import { MobileContext } from "../../Context/MobileContext";
 
 const { Text, Title } = Typography;
 
@@ -21,18 +22,22 @@ const Section = ({
   navId,
   message,
 }: SectionProps) => {
+  const { isMobile } = useContext(MobileContext);
+
   return (
     <Row
       id={navId}
       className={`section-row`}
-      style={{ flexDirection: img?.left ? "row" : "row-reverse" }}
+      style={{
+        flexDirection: isMobile ? "column" : img?.left ? "row" : "row-reverse",
+      }}
       align={navId === "contact" ? "top" : "middle"}
       justify="center"
     >
       {img && (
         <Col
           style={{ padding: "3em", display: "flex", justifyContent: "center" }}
-          span={12}
+          span={isMobile ? 24 : 12}
         >
           <Image
             style={{ maxHeight: "600px" }}
@@ -43,16 +48,22 @@ const Section = ({
       )}
       <Col
         style={{
-          padding: img
+          padding: isMobile
+            ? "1em"
+            : img
             ? img?.left
               ? "3.5em 5em 3.5em 1em"
               : "3.5em 1em 3.5em 5em"
             : "5em 11em",
+          textAlign: isMobile ? "center" : "start",
         }}
-        span={img ? 11 : 24}
+        span={isMobile ? 24 : img ? 11 : 24}
         offset={img ? 1 : 0}
       >
-        <Title level={navId === "contact" ? 2 : 1}>
+        <Title
+          style={{ textAlign: isMobile ? "center" : "start" }}
+          level={navId === "contact" ? 2 : 1}
+        >
           {icon} {title}
         </Title>
         <Text>{message}</Text>
